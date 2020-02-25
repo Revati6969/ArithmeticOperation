51a3ff6ded47a4d389feb305cfb2492f71630774
Revati Tekale <revitekale1910@gmail.com>
2020-02-25 23:04:44 +0530

    ADDED:Stored result in dictionary

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -1,11 +1,22 @@
 ##!/bin/bash -x
 echo "Arithmetic Computation"
 
+declare -A calculations
+
 echo "Enter three inputs: "
 read a
 read b
 read c
+
 result1=$(( $a+$b*$c ))
 result2=$(( $a*$b+$c ))
 result3=$(( $c+$a/$b ))
 result4=$(( $a%$b+$c ))
+
+calculations[result1]="$result1"
+calculations[result2]="$result2"
+calculations[result3]="$result3"
+calculations[result4]="$result4"
+ 
+echo ${calculations[@]}
+echo ${!calculations[@]}
