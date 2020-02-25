32f10e484a5557e0b1f250f8ca114b19bf8d9c90
Revati Tekale <revitekale1910@gmail.com>
2020-02-25 22:58:26 +0530

    ADDED:Computed fourth operation

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+##!/bin/bash -x
 echo "Arithmetic Computation"
 
 echo "Enter three inputs: "
@@ -8,4 +8,4 @@ read c
 result1=$(( $a+$b*$c ))
 result2=$(( $a*$b+$c ))
 result3=$(( $c+$a/$b ))
-
+result4=$(( $a%$b+$c ))
